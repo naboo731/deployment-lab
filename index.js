bd4a1db808b18b807d14f45d64aa6b2b62ef12f4
Nichole Boothman <nbooth731@gmail.com>
2021-12-15 16:49:56 -0500

    added text

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname, '../index.html'))
-// })
+
 app.use('/',express.static(path.join(__dirname, 'public')))
 
 const port = process.env.PORT || 4005
