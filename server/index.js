b2d0617323e6e6d2c185ee26888748a329296fee
Nichole Boothman <nbooth731@gmail.com>
2021-12-15 16:30:47 -0500

    created public folder

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../index.html'))
-})
-app.use('/', express.static(path.join(__dirname, 'public')))
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../index.html'))
+// })
+app.use(express.static(path.join(__dirname, 'public')))
 
 const port = process.env.PORT || 4005
 
