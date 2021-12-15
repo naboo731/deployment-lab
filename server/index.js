cf8b7c31ab6ade94c568db1bea1b41d93e020201
Nichole Boothman <nbooth731@gmail.com>
2021-12-15 16:06:16 -0500

    changes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const app = express()
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+  })
 
 const port = process.env.PORT || 4005
 
