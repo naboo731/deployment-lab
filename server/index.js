bb5dffdfb65e8d18b7e6edba2e0a1ab60b88fbcc
Nichole Boothman <nbooth731@gmail.com>
2021-12-15 16:11:58 -0500

    changes

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.get('/', function(req, res) {
+app.get('/js', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
-app.get('/', (req, res) => {
+app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '../styles.css'))
   })
 
